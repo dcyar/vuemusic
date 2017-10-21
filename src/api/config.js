d3ac2a703deca39e6a4f9c393b5b07139b8a21a4
Jhon Marco Achata Limachi <jmal_444@hotmail.com>
2017-10-21 10:36:31 -0500

    api configs

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'vuemusic',
+    apiKey: 'eea71da30ed14202650eeb869864b08a',
+    secret: '3fe558adf1ea9ec0e73e7398f3f232b7',
+    registeredTo: 'dcyar'
+}
+
+// Nombre de aplicación	vuemusic
+// Clave API	eea71da30ed14202650eeb869864b08a
+// Secreto compartido	3fe558adf1ea9ec0e73e7398f3f232b7
+// Registrado para	dcyar
